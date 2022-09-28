de3cb106b113873bcd8a0e7f288350e3b8d48720
andreresende36 <andreresende36@gmail.com>
2022-09-28 15:23:13 -0300

    Correção código requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,34 +22,39 @@ function generatePhoneNumber(array) {
     }
   }
 
-  for (let index = 0; index < array.length; index += 1) {
-    if (array.length !== 11) {
-      return 'Array com tamanho incorreto';
-      break;
-    }
-    else if (array[index] < 0 || array[index] > 9 || contador >= 3) {
-      return 'não é possível gerar um número de telefone com esses valores';
-      break;
-    }
-    else if (index >= 0 && index <= 1) {
-      ddd.push(array[index]);
-    }
-    else if (index >= 2 && index <= 6) {
-      primeiraSecao.push(array[index]);
-    }
-    else if (index >= 7 && index <= 9) {
-      segundaSecao.push(array[index]);
-    }
-    else {
-      segundaSecao.push(array[index]);
-      numeroTelefone = '(' + ddd.join('') + ') ' + primeiraSecao.join('') + '-' + segundaSecao.join('');
-      return numeroTelefone;
+  if (array.length === 0) {
+    return 'Array com tamanho incorreto.';
+  }
+  else if (array.length > 0) {
+    for (let index = 0; index < array.length; index += 1) {
+      if (array.length !== 11) {
+        return 'Array com tamanho incorreto.';
+        break;
+      }
+      else if (array[index] < 0 || array[index] > 9 || contador >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+        break;
+      }
+      else if (index >= 0 && index <= 1) {
+        ddd.push(array[index]);
+      }
+      else if (index >= 2 && index <= 6) {
+        primeiraSecao.push(array[index]);
+      }
+      else if (index >= 7 && index <= 9) {
+        segundaSecao.push(array[index]);
+      }
+      else {
+        segundaSecao.push(array[index]);
+        numeroTelefone = '(' + ddd.join('') + ') ' + primeiraSecao.join('') + '-' + segundaSecao.join('');
+        return numeroTelefone;
+      }
     }
   }
 }
 
-// let arrayTeste = [1, 1, 1, 4, 5, 6, 7, 8, 9, 0, 1];
-// console.log(generatePhoneNumber(arrayTeste));
+let arrayTeste = [];
+console.log(generatePhoneNumber(arrayTeste));
 
 // Desafio 12
 function triangleCheck() {
